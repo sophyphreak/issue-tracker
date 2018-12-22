0798adccfc5eefedad8901b51acc1d8d231e8c8c
Andrew <arhorn@smcm.edu>
2018-12-23 03:34:40 +0700

    got POST new issue working

--- routes/api.js
+++ routes/api.js
@@ -1,41 +1,60 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
+// const expect = require('chai').expect;
+// const MongoClient = require('mongodb');
+// const ObjectId = require('mongodb').ObjectID;
+const moment = require('moment');
+const { mongoose } = require('../db/mongoose');
+const _ = require('lodash');
+const { Issue } = require('../models/issue');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+// const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app) {
+module.exports = app => {
+  app
+    .route('/api/issues/:project')
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .post(function (req, res){
-      var project = req.params.project;
-      
+
+    .post(async (req, res) => {
+      try {
+        // console.log('asfsd')
+        const project = req.params.project;
+        let body = _.pick(req.body, [
+          'issue_title',
+          'issue_text',
+          'created_by',
+          'assigned_to',
+          'status_text'
+        ]);
+        body.project = project;
+        body.created_on = moment();
+        body.updated_on = moment();
+        body.open = true;
+        const issue = new Issue(body);
+        const doc = await issue.save();
+        res.send(doc);
+        // console.log('asdlfkjsad')
+      } catch (e) {
+        res.status(400).send(e);
+      }
     })
-    
-    .put(function (req, res){
+
+    .put(function(req, res) {
       var project = req.params.project;
-      
     })
-    
-    .delete(function (req, res){
+
+    .delete(function(req, res) {
       var project = req.params.project;
-      
     });
-    
 };
--- server.js
+++ server.js
@@ -1,37 +1,34 @@
-"use strict";
+'use strict';
 require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const expect = require("chai").expect;
-const cors = require("cors");
-const helmet = require("helmet");
-const lodash = require("lodash");
-const moment = require('moment');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
+const helmet = require('helmet');
 
-const apiRoutes = require("./routes/api.js");
-const fccTestingRoutes = require("./routes/fcctesting.js");
-const runner = require("./test-runner");
-const { Issue } = require('./models/issue');
+const apiRoutes = require('./routes/api.js');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use(helmet());
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({ origin: "*" })); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sample front-end
-app.route("/:project/").get(function(req, res) {
-  res.sendFile(process.cwd() + "/views/issue.html");
+app.route('/:project/').get(function(req, res) {
+  res.sendFile(process.cwd() + '/views/issue.html');
 });
 
 //Index page (static HTML)
-app.route("/").get(function(req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
+app.route('/').get(function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
 //For FCC testing purposes
@@ -40,41 +37,45 @@ fccTestingRoutes(app);
 //Routing for API
 apiRoutes(app);
 
-app.post('/api/issues/:projectname', async (req, res) => {
-  try {
-    const project = req.params.projectname;
-    let body = _pick(req.body, ['issue_title', 'issue_text', 'created_by', 'assigned_to', 'status_text']);
-    body.project = project;
-    body.created_on = moment();
-    body.updated_on = moment();
-    body.open = true;
-    const issue = new Issue(body);
-    const doc = await issue.save();
-    res.send(doc);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res
     .status(404)
-    .type("text")
-    .send("Not Found");
+    .type('text')
+    .send('Not Found');
+});
+
+app.use((err, req, res, next) => {
+  let errCode, errMessage;
+
+  if (err.errors) {
+    // mongoose validation error
+    errCode = 400; // bad request
+    const keys = Object.keys(err.errors);
+    // report the first validation error
+    errMessage = err.errors[keys[0]].message;
+  } else {
+    // generic or custom error
+    errCode = err.status || 500;
+    errMessage = err.message || 'Internal Server Error';
+  }
+  res
+    .status(errCode)
+    .type('txt')
+    .send(errMessage);
 });
 
 //Start our server and tests!
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + listener.address().port);
-  if (process.env.NODE_ENV === "test") {
-    console.log("Running Tests...");
+  console.log('Listening on port ' + listener.address().port);
+  if (process.env.NODE_ENV === 'test') {
+    console.log('Running Tests...');
     setTimeout(function() {
       try {
         runner.run();
       } catch (e) {
         var error = e;
-        console.log("Tests are not valid:");
+        console.log('Tests are not valid:');
         console.log(error);
       }
     }, 3500);
