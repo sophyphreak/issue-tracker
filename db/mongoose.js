e3549095e7ce43985572541b281848b58190e3b4
Andrew <arhorn@smcm.edu>
2018-12-25 01:22:58 +0700

    mongoose no longer uses deprecated way to connect to server

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,7 +5,12 @@ if (process.env.NODE_ENV === 'test') {
   mongoose.connect(`mongodb://localhost:27017/IssueTrackerTest`);
 } else {
   mongoose.connect(
-    `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ds155293.mlab.com:55293/issue-tracker`
+    `mongodb://${process.env.DB_USER}:${
+      process.env.DB_PASS
+    }@ds155293.mlab.com:55293/issue-tracker`,
+    {
+      useNewUrlParser: true
+    }
   );
 }
 
